f01f8ece3e649a68f9395fb57a0c80f86686d7bf
wkdtpzld <slwhswk9@gmail.com>
2024-06-27 03:50:10 +0900

    Error Page

---
+++ src/app/jks/error/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ErrorTrigger from "@/component/JKS/misc/ErrorTrigger/ErrorTrigger";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <ErrorTrigger/>
+      Hello
+    </div>
+  )
+}
+
+export default ErrorPage
