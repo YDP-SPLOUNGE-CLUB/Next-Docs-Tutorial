cef7c5217f484bcfb01afce5c1ad451b5230ddcb
Vespucci95 <donglonk@naver.com>
2024-06-28 16:49:38 +0900

    ✨ Error 학습

---
+++ src/app/lhs/routing/error-handling/page.tsx
@@ -0,0 +1,9 @@
+import ErrorButton from '../../../../../components/lhs/ErrorButton';
+
+export default function ErrorHandlingPage() {
+  return (
+    <ErrorButton>
+      Throw Error
+    </ErrorButton>
+  )
+}
