024b7457ef3836bed6275ced4b0bdcaeb8b7924d
Arthur <arthur@aiskorea.co.kr>
2024-07-25 16:10:56 +0900

    4주차

--- src/app/jks/interceptor/page.tsx
+++ src/app/jks/interceptor/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div style={{zIndex: 100}}>
       안보입니다.
-      <Link href={'/jks/new/item'}> 클릭</Link>
+      <Link href={'/jks/news/item'}> 클릭</Link>
     </div>
   )
 }
